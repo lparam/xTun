6a76154c88fadd96d116ee479e30e6e93d5e96c8
lparam <lparam@users.noreply.github.com>
2019-08-12 23:17:29 +0800

    remove unused function

--- src/util.h
+++ src/util.h
@@ -16,7 +16,6 @@
 void dump_hex(const void *data, uint32_t len, char *title);
 int resolve_addr(const char *buf, int port, struct sockaddr *addr);
 int ip_name(const struct sockaddr *ip, char *name, size_t size);
-int add_route(const char *name, const char *address, int prefix);
 int create_socket(int type, int reuse);
 pid_t gettid();
 int read_size(uint8_t *buffer);

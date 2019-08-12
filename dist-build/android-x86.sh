7ee3e8f54854c6e2630759f389d29c5f2616affa
lparam <lparam@users.noreply.github.com>
2019-08-12 23:21:34 +0800

    upgrade androind build scripts

--- dist-build/android-x86.sh
+++ dist-build/android-x86.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TARGET_ARCH=i686
-export CFLAGS="-Os -march=${TARGET_ARCH}"
-ARCH=x86 HOST_COMPILER=i686-linux-android "$(dirname "$0")/android-build.sh"
+export CFLAGS="-Os -march=${TARGET_ARCH} -fPIC"
+ARCH=x86 HOST_COMPILER=i686-linux-android CC=i686-linux-android21-clang "$(dirname "$0")/android-build.sh"

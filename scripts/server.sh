42e852b12cba9db0763eb44c8dbf18ff45acbab6
lparam <lparam@users.noreply.github.com>
2020-01-05 23:32:10 +0800

    update scripts

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 IFACE=tun0
-CIDR=10.0.0.1/24
-CHAIN=xTun
-COMMENT=xTun
+CIDR=10.0.1.1/24
 PORT=1082
 PASSWORD=password
+
+CHAIN=xTun
+COMMENT=xTun
 
 start() {
     xTun -i $IFACE -I $CIDR -k $PASSWORD -s -p $PORT
@@ -22,15 +23,20 @@ shutdown() {
     xTun --signal quit
 }
 
+restart() {
+    stop
+    start
+}
+
 net_start() {
     # Turn on IP forwarding
-    sysctl -w net.ipv4.ip_forward=1 > /dev/null 2>&1
+    sysctl -w net.ipv4.ip_forward=1 >/dev/null
 
     # turn on NAT over VPN
     if !(iptables-save -t nat | grep -q $CHAIN); then
         iptables -t nat -A POSTROUTING -s $CIDR ! -d $CIDR -m comment --comment $COMMENT -j MASQUERADE
     fi
-    iptables -N $CHAIN > /dev/null 2>&1 || (
+    iptables -N $CHAIN >/dev/null 2>&1 || (
         iptables -D FORWARD -j $CHAIN
         iptables -F $CHAIN
         iptables -Z $CHAIN
@@ -40,7 +46,7 @@ net_start() {
     iptables -A FORWARD -j $CHAIN
 
     # Turn on MSS fix (MSS = MTU - TCP header - IP header)
-    iptables -t mangle -N $CHAIN > /dev/null 2>&1 || (
+    iptables -t mangle -N $CHAIN >/dev/null 2>&1 || (
         iptables -t mangle -D FORWARD -j $CHAIN
         iptables -t mangle -F $CHAIN
         iptables -t mangle -Z $CHAIN
@@ -51,23 +57,24 @@ net_start() {
 
 net_stop() {
     # Turn off NAT over VPN
-    iptables -t nat -D POSTROUTING -s $CIDR ! -d $CIDR -m comment --comment $COMMENT -j MASQUERADE > /dev/null 2>&1
+    iptables -t nat -D POSTROUTING -s $CIDR ! -d $CIDR -m comment --comment $COMMENT -j MASQUERADE 2>/dev/null
 
-    iptables -D FORWARD -j $CHAIN > /dev/null 2>&1
-    iptables -F $CHAIN > /dev/null 2>&1
-    iptables -X $CHAIN > /dev/null 2>&1
+    iptables -D FORWARD -j $CHAIN 2>/dev/null
+    iptables -F $CHAIN 2>/dev/null
+    iptables -X $CHAIN 2>/dev/null
 
     # Turn off MSS fix (MSS = MTU - TCP header - IP header)
-    iptables -t mangle -D FORWARD -j $CHAIN > /dev/null 2>&1
-    iptables -t mangle -F $CHAIN > /dev/null 2>&1
-    iptables -t mangle -X $CHAIN > /dev/null 2>&1
+    iptables -t mangle -D FORWARD -j $CHAIN 2>/dev/null
+    iptables -t mangle -F $CHAIN 2>/dev/null
+    iptables -t mangle -X $CHAIN 2>/dev/null
 }
 
 show_help() {
     echo "Usage: $ProgName <command> [options]"
     echo "Commands:"
-    echo "    start   start tun"
-    echo "    stop    stop tun"
+    echo "    start     start tun"
+    echo "    stop      stop tun"
+    echo "    restart   restart tun"
     echo ""
     echo "For help with each command run:"
     echo "$ProgName <command> -h|--help"
